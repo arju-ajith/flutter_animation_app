86f4b37c941410fc99e5a71cdf76e55db10001c0
Jeeva-Itero <jeeva@itero.ie>
2022-12-28 14:07:12 +0530

    Initial commit

---
+++ lib/view/tween_animation_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class TweenAnimationScreen extends StatefulWidget {
+  const TweenAnimationScreen({super.key});
+
+  @override
+  State<TweenAnimationScreen> createState() => _TweenAnimationScreenState();
+}
+
+class _TweenAnimationScreenState extends State<TweenAnimationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
